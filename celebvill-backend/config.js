b3eb91d9ce6a42392a652422a0fe1d714a217d58
Therdsakmax050941 <maxza323@hotmail.co.th>
2023-11-07 19:04:05 +0700

    backend

---
+++ celebvill-backend/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // database
+  MONGODB_USERNAME: "admin01",
+  MONGODB_PASSWORD: "vbXamfqlqbqq9fos",
+  MONGODB_DB_NAME: "user_admin",
+
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "VPHImtaWjm0xl6vaEMLD2xO7vk4Kfetj",
+
+  //gmail credentials for send email
+  EMAIL: "celebvill.idea@gmail.com",
+  PASSWORD: "Celeb@!1234",
+
+  //secret key for jwt
+  JWT_SECRET: "FDYnXvdfs4rd9I",
+
+  SERVER_PATH: "https://api.celebvill.com/", // example :https://sdgserhgdhbrtsfgs.com/
+
+  // firebase server key for notification
+  SERVER_KEY: "m0GKr-rVa2Czn712ohXCqHG-fOpW-OGEWJPNQ0JUO54",
+};
